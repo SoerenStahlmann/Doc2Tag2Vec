745231e6d707856442bf33dc29d7efc27b760d9f
Soeren <mannstahl@hotmail.de>
2020-01-06 11:34:05 +0100

    Documented code

--- Doc2Tag2Vec.py
+++ Doc2Tag2Vec.py
@@ -12,18 +12,19 @@ from scipy.spatial.distance import cosine
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 
 
-#TODO: documentation
-
-
 class Doc2Tag2Vec(Doc2Vec):
     
     
     def __init__(self, **kwargs):
-        
+        """
+        Model initializer from gesim..models.doc2vec
+        """
         super(Doc2Tag2Vec, self).__init__(**kwargs)
         
     
-    def _data_loader(self, file_path, use_csv = True):
+    def _data_loader(self, 
+                     file_path: str, 
+                     use_csv  : bool = True):
         
         corpus = []
         
@@ -45,9 +46,30 @@ class Doc2Tag2Vec(Doc2Vec):
                     
         return corpus
                     
-                    
+    
     def load_training_data(self, **kwargs):
+        """Reads all text files from a given directory.
         
+        file_path -> str : Path to the the directory containing all documents.
+        use_csv   -> bool: If the files inside the directory are csv or txt 
+                           files
+        documents -> List[TaggesDocument]: Can be simply a list of 
+                                           TaggedDocument elements, but for larger 
+                                           corpora, consider an iterable 
+                                           that streams the documents directly 
+                                           from disk/network
+        corpus_file -> str    : Path to a corpus file in LineSentence format. You 
+                                may use this argument instead of documents to get 
+                                performance boost. Only one of documents or 
+                                corpus_file arguments need to be passed 
+                               (not both of them)
+        update -> bool        : If true, the new words in documents will be added to 
+                                model’s vocab.
+        progress_per -> int   : Indicates how many words to process before 
+                                showing/updating the progress.
+        keep_raw_vocab -> bool: If not true, delete the raw vocabulary after 
+                                the scaling is done and free up RAM.
+        """
         corpus = list(self._data_loader(**kwargs))
         
         self.build_vocab(corpus)
@@ -55,8 +77,15 @@ class Doc2Tag2Vec(Doc2Vec):
         return corpus
     
     
-    # TODO: append to model.train method
-    def train(self, file_path='keywords.txt', sep='\n', **kwargs):
+    def train(self,
+              file_path : str ='keywords.txt',
+              sep       : str = '\n',
+              **kwargs):
+        """
+        Override the gensim.models.doc2vec train method. After running the 
+        train method this method creates adn returns a Dictionary containing all
+        keyword vectors.
+        """
         
         print("Training model...")
         super(Doc2Tag2Vec, self).train(**kwargs)
@@ -88,7 +117,12 @@ class Doc2Tag2Vec(Doc2Vec):
         return self.keyword_vec_dict
     
     
-    def predict_keywords(self, document_id, nbr_of_keywords = -1):
+    def predict_keywords(self, 
+                         document_id     : str, 
+                         nbr_of_keywords : int = -1):
+        """
+        Returns the most similar keyword(s) for a given document id.
+        """
         
         document_keywords = {}
         
@@ -123,9 +157,9 @@ if __name__ == "__main__":
                 workers=64,
                 negative=1)
     
-    corpus =  model.load_training_data(file_path="doctest", use_csv=False)
+    #corpus =  model.load_training_data(file_path="doctest", use_csv=False)
     
-    model.train(documents=corpus, total_examples=model.corpus_count,  epochs=model.epochs, file_path="doctest/sachdiskreptoren_preprocessed.txt")
+    #model.train(documents=corpus, total_examples=model.corpus_count,  epochs=model.epochs, file_path="doctest/sachdiskreptoren_preprocessed.txt")
     
         
         
